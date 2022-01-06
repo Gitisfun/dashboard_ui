8551b896846dc9b0788547c2686df72ebff6695b
christian <christian@wedoit.be>
2022-01-06 22:01:04 +0100

    Add TODOs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 // TODO: Datum fixen, telt altijd een dag terug in aankoop, verkoop en creditnota
 // TODO: Tabel toggle aangepast door
 // TODO: Update show when last updated and by who
-// TODO: Add bestetlling en order nr in create and use api call for correct index
+// TODO: Add bestelling en order nr in create and use api call for correct index
 // TODO: delete console logs
 // TODO: Facturen titel beter maken bij zowel aankoop als verkoop
 // TODO: Opmerking order aanpassen qua naam voor AK, VK, CR
